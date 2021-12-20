4ed7205566c332898538185613e0ccdf35813322
AnikSq <anikr23456@gmail.com>
2021-12-20 19:37:50 +0530

    Pushing stuff out 2

--- scriptTwo.js
+++ scriptTwo.js
@@ -25,7 +25,7 @@ let numOne = []
 let numTwo = []
 let numTotal = []
 let allOperators = []
-val = [false]
+let val = [false]
 let checker = []
 
 
@@ -39,19 +39,52 @@ function numbers(main , val){
         }
     })
 }
+
+function backFunc(main){
+    main.addEventListener('click',le=>{
+        if (numOne.length > 1){
+            let lastArr = numOne.at(-1)
+            splice(lastArr,1)
+        }else{
+            let lastArr = numTwo.at(-1)
+            splice(lastArr,1)
+        }
+    })
+}
+
+
+
+function clearFunc(main){
+    main.addEventListener('click',let=>{ // clearing all the registered values expect for val
+    numOne = []
+    numTwo = []
+    numTotal = []
+    allOperators = []
+    checker = []
+    val.push(false)
+    })
+}
+
+function join(main){
+    let giveBack = ""
+    for(let i=0;i<main.length;i++){ // joins 2 strings
+        giveBack += main[i]
+    }
+    return giveBack
+}
        
 
-function operator(main , val){
+function operatorPlus(main , val){
     main.addEventListener('click',let=>{ // works when + is clicked
-       val.push(true) // makes the val true , to make the numbers go to numTwo
-       if (checker.at(-1) == main.textContent){
+       if (checker.at(-1) == main.textContent || checker.length == 0){ // makes sure 2 plus aren't being used 
         return;
        }else{
+        val.push(true) // makes the val true , to make the numbers go to numTwo
         allOperators.push(main.textContent) // saves what operator is being used
        }
        checker.push(main.textContent); // pushes everything for reference
        if (allOperators.length > 1){ // if there are more than one operator in the allOperators variable then execute this statement
-          let temp = parseInt(numOne) + parseInt(numTwo); // a temp number to store the sum
+          let temp = parseInt(join(numOne)) + parseInt(join(numTwo)); // a temp number to store the sum
           numOne = [] // since the work of this array is done it is set to empty
           numTwo = [] // same set as empty
           numOne.push(temp) // pushes the newly made number to numOne
@@ -59,5 +92,75 @@ function operator(main , val){
     })
 }
 
+
+function operatorMinus(main , val){
+    main.addEventListener('click',let=>{ // works when + is clicked
+        if (checker.at(-1) == main.textContent || checker.length == 0){ // makes sure 2 plus aren't being used 
+         return;
+        }else{
+        val.push(true) // makes the val true , to make the numbers go to numTwo
+        allOperators.push(main.textContent) // saves what operator is being used
+        }
+        checker.push(main.textContent); // pushes everything for reference
+        if (allOperators.length > 1){ // if there are more than one operator in the allOperators variable then execute this statement
+           let temp = parseInt(join(numOne)) - parseInt(join(numTwo)); // a temp number to store the sum
+           numOne = [] // since the work of this array is done it is set to empty
+           numTwo = [] // same set as empty
+           numOne.push(temp) // pushes the newly made number to numOne
+        }
+     })
+
+}
+
+function operatorMulti(main , val){
+    main.addEventListener('click',let=>{ // works when + is clicked
+        if (checker.at(-1) == main.textContent || checker.length == 0){ // makes sure 2 plus aren't being used 
+         return;
+        }else{
+        val.push(true) // makes the val true , to make the numbers go to numTwo
+        allOperators.push(main.textContent) // saves what operator is being used
+        }
+        checker.push(main.textContent); // pushes everything for reference
+        if (allOperators.length > 1){ // if there are more than one operator in the allOperators variable then execute this statement
+           let temp = (parseInt(join(numOne)) * parseInt(join(numTwo))); // a temp number to store the sum
+           numOne = [] // since the work of this array is done it is set to empty
+           numTwo = [] // same set as empty
+           numOne.push(temp) // pushes the newly made number to numOne
+        }
+     })
+}
+
+function operatorDivision(main,val){
+    main.addEventListener('click',let=>{ // works when + is clicked
+        if (checker.at(-1) == main.textContent || checker.length == 0){ // makes sure 2 plus aren't being used 
+        return;
+        }else{
+        val.push(true) // makes the val true , to make the numbers go to numTwo
+        allOperators.push(main.textContent) // saves what operator is being used
+        }
+        checker.push(main.textContent); // pushes everything for reference
+        if (allOperators.length > 1){ // if there are more than one operator in the allOperators variable then execute this statement
+           let temp = parseInt(join(numOne)) / parseInt(join(numTwo)); // a temp number to store the sum
+           numOne = [] // since the work of this array is done it is set to empty
+           numTwo = [] // same set as empty
+           numOne.push(temp) // pushes the newly made number to numOne
+        }
+     })
+}
+
 numbers(seven , val)
-operator(plus , val);
+numbers(nine,val)
+numbers(eight,val)
+numbers(six,val)
+numbers(five,val)
+numbers(four,val)
+numbers(three,val)
+numbers(two,val)
+numbers(one,val)
+numbers(zero,val)
+operatorPlus(plus , val);
+operatorMinus(minus , val);
+operatorMulti(multi,val)
+operatorDivision(division,val)
+clearFunc(clear);
+backFunc(back);
